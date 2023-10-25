e85b73fdcc4e210c44549972a874581ebba69125
ctkim <nkdevil@naver.com>
2023-10-25 09:50:21 +0900

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 import path from 'path'
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 const config: UserConfig = {
 	resolve: {
 		alias: {
